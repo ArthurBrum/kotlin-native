3baa8e921b5209952693f93dbfc2e3a028c160c9
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-02-04 13:13:39 +0700

    [RTTI][Objective-C] Omit RTTI for external Obj-C classes

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/RTTIGenerator.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/RTTIGenerator.kt
@@ -195,11 +195,13 @@ internal class RTTIGenerator(override val context: Context) : ContextUtils {
 
         val instanceSize = getInstanceSize(bodyType, className)
 
-        val superTypeOrAny = irClass.getSuperClassNotAny() ?: context.ir.symbols.any.owner
+        val superTypeOrAny = irClass.getSuperClassNotAny().let { superType ->
+            if (superType == null || !superType.requiresRtti()) context.ir.symbols.any.owner else superType
+        }
         val superType = if (irClass.isAny()) NullPointer(runtime.typeInfoType)
                 else superTypeOrAny.typeInfoPtr
 
-        val implementedInterfaces = irClass.implementedInterfaces
+        val implementedInterfaces = irClass.implementedInterfaces.filter { it.requiresRtti() }
 
         val interfaces = implementedInterfaces.map { it.typeInfoPtr }
         val interfacesPtr = staticData.placeGlobalConstArray("kintf:$className",
